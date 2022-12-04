fd904a9a923dc597ce07a2354f1f0a832d683581
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2022-12-04 08:47:16 +0100

    Add files via upload

---
+++ Zad2 w 3/Zad2 w 3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Zad2_w_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int n;
+
+            int s = 0;
+
+            int i = 1;
+
+            Console.WriteLine("Ile liczb chcesz dodać?");
+
+            n=int.Parse(Console.ReadLine());
+
+            while (i <=n)
+            { Console.WriteLine("i = " + i + "\t s = " + s);
+                s += i;
+                i++;
+            }
+            Console.WriteLine("Suma = " + s);
+            
+                    
+            
+        }
+    }
+}
